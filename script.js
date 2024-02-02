8055af088a6cc8c35310982adc437f77e2631810
olawes04 <144459640+olawes04@users.noreply.github.com>
2024-02-02 15:27:01 +0000

    Quiz done

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function loaded() {
     console.log("The page has now loaded");
 }
+function showRight(){
+    document.getElementById("feedback-area").innerHTML= "Correct";
+}
+function showWrong(){
+    document.getElementById("feedback-area").innerHTML= "You bufoon";
+}
